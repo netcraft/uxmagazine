ebced3cbbadc4ee88770cad02bc33b9bbd0aa0d2
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2014-01-09 19:21:40 +0200

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+ 
+  $(".courses .gallery").owlCarousel({
+  	singleItem: true,
+  	itemForcedWidth: 394,
+
+  	afterMove: function() {
+  		var self = this;
+
+  		$(".owl-item").removeClass("current");
+  		$(".owl-item:nth-child("+(this.currentItem+1)+")").addClass("current");
+
+  		$(".owl-item").each(function(index, element) {
+  			var transform = {translateX: 0, scale: 1};
+  			var zIndex = 50;
+
+  			if (index < self.currentItem) {
+  				if (index == self.currentItem-1) {
+  					transform.translateX = -20;
+  					transform.scale = 0.9;
+  					zIndex -= 1;
+  				} else {
+  					transform.translateX = 20;
+  					transform.scale = 0.9;
+  					zIndex -= 2;
+  				}
+  			} else if (index > self.currentItem) {
+				transform.translateX = -132-(((index-1) - self.currentItem) * 40);
+				transform.scale = 0.9;
+  				zIndex -= (index - self.currentItem);
+  			} else {
+				transform.translateX = -78;
+				transform.scale = 1;
+  			}
+
+  			$(element).find(".gallery-item")
+  					  .css("transform", "translateX("+transform.translateX+"px) scale("+transform.scale+")");
+  			$(element).css("z-index", zIndex);
+  		});
+  	}
+  });
+
+  var owl = $(".owl-carousel").data('owlCarousel');
+  owl.goTo(1);
+ 
+});
